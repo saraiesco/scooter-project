2cfaa33f556b31b0c62f0be9e31245dff003da9e
saraiesco <saraiescobar@google.com>
2023-09-29 15:14:26 -0500

    scooter methods

--- src/Scooter.js
+++ src/Scooter.js
@@ -9,6 +9,22 @@ class Scooter{
     this.charge = 100;
     this.isBroken = false;
   }
+
+  rent(user){
+    if(this.charge <= 20){
+      throw new Error ("scooter needs to charge");
+    }else if(this.isBroken === true){
+      throw new Error ('scooter needs repair');
+    }
+    else{
+      this.station = null;
+      this.user = user;
+    }
+  }
+
+  dock(station){
+
+  }
 }
 
 
