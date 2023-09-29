95622e5ab1a3aa5f4306197d1f1a1a8ce81510d5
saraiesco <saraiescobar@google.com>
2023-09-29 14:09:56 -0500

    user constructor

--- src/User.js
+++ src/User.js
@@ -1,7 +1,10 @@
 class User {
   // User code here
   constructor(username, password, age, loggedIn){
-
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = false;
   }
 }
 
