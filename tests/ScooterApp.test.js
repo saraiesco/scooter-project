f88b5c14f4b7da75ee435880034849d9dbdc8567
saraiesco <saraiescobar@google.com>
2023-10-03 08:44:43 -0500

    git check

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -13,10 +13,56 @@ describe("registerUser method tests", () => {
   });
 });
 
-// log in
+// login User
+describe("login method tests", () => {
+  test("Should login user in scooterApp", () => {
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    //
+    expect(response).toBeInstanceOf(User);
+  });
+});
+// logout User
+describe("logout method tests", () => {
+  test("Should logout user in scooterApp", () => {
+    //
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
 
-// log out
-
-// rent scooter
+//create scooter
+describe("create scooter method tests", () => {
+  test("Should create and return new scooter", () => {
+    //
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
 
 // dock scooter
+describe("dock scooter method tests", () => {
+  test("Should dock scooter at station", () => {
+    //
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
+
+
+// rent scooter
+describe("rent scooter method tests", () => {
+  test("Should rent scoot out to user", () => {
+    //
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
+
+//print
+describe("rent scooter method tests", () => {
+  test("Should return instance of User", () => {
+    //
+    let response = scooterApp.registerUser("Joe Bloggs", "test123", 21);
+    expect(response).toBeInstanceOf(User);
+  });
+});
