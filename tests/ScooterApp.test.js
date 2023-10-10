d8e073f4e3d48ad102cf91c3a87d4571b617a2e1
saraiesco <saraiescobar@google.com>
2023-10-09 20:05:45 -0500

    one more scooterApp Jest passing method

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -1,61 +1,94 @@
-const Scooter = require('../src/Scooter')
-const User = require('../src/User')
-const ScooterApp = require('../src/ScooterApp')
+const User = require('./User')
+const Scooter = require('./Scooter')
 
-const scooterApp = new ScooterApp();
-// ScooterApp tests here
+class ScooterApp {
+  // ScooterApp code here
+    constructor(stations, registeredUsers){
+    this.stations = { 'chicago' : [], 'multiverse' : [], 'google' : [] };
+    this.registeredUsers = {};
+    // registeredUsers = { shami : { username: ‘shami1’ , password: ‘milo’ , age : 17, logginIn: true} ,sarai : { username: ‘sarai’ , password: ‘hamilton’ , age : 10, logginIn: false} }
 
-// register user
-describe("registerUser method tests", () => {
-  test("Should return instance of User", () => {
-    let response = scooterApp.registerUser("test123" , "Joe Bloggs", 21);
-    expect(response).toBeInstanceOf(User);
-  });
-});
+  }
 
-// login User
-describe("login method tests", () => {
-  test("Should login user in scooterApp", () => {
-    scooterApp.loginUser("Joe Bloggs", "test123")
-    expect(scooterApp.registeredUsers["Joe Bloggs"]).toBe(true);
-  });
-});
+  registerUser(username,password, age){
+    if(this.registeredUsers[username]){
+      throw('already registered')
+    }else if(age < 18){
+      throw ('too young to register')
+    }
+      const user = new User(username, password, age);
+      this.registeredUsers[username] = user;
+      console.log('user has been registered')
+      return this.registeredUsers[username];
+  }
 
-// logout User
-describe("logout method tests", () => {
-  test("Should logout user in scooterApp", () => {
-    //
-    scooterApp.logoutUser("Joe Bloggs")
-    expect(scooterApp.registeredUsers["Joe Bloggs"].toBe(false))
-  });
-});
+  loginUser(username, password){
+      if(this.registeredUsers[username] === username && this.registeredUsers[password] === password){
+        User.login(password)
+        console.log(`${username} has been logged in`)
+      }else{
+        throw new Error('Username or password is incorrect')
+      }
+  }
 
-//create scooter
-describe("create scooter method tests", () => {
-  test("Should create and return new scooter", () => {
-    //
-    expect(scooterApp.createScooter("chicago")).toBeInstanceOf(Scooter)
-  });
-});
-
-// dock scooter
-describe("dock scooter method tests", () => {
-  test("Should dock scooter at station", () => {
-    //
-    scooterApp.dockScooter("scoot4567", "chicago")
-    expect(["scoot4567"].station).toBe("chicago")
-    expect(["scoot4567"].user).toBe(null)
-  });
-});
+  logoutUser(username){
+    for(property in this.registeredUsers){
+      if(this.registeredUsers[property][username]){
+        User.logout()
+        console.log('user is logged out')
+      }else{
+        throw new Error('no such user is logged in')
+      }
+    }
+  }
 
 
-// rent scooter
-describe("rent scooter method tests", () => {
-  test("Should rent scoot out to user", () => {
-    //
-    scooterApp.rentScooter("scoot4567", "Joe Bloggs")
-    expect(["scoot4567"].station).toBe(null)
-    expect(["scoot4567"].user).toBe("Joe Bloggs")
-  });
-});
+  createScooter(station){
+      if(!this.stations[station]){
+        throw new Error('no such station')
+      }else{
+          const scooter = new Scooter(station);
+          this.stations[station].push(scooter)
+        console.log('created new scooter')
+      return scooter
+      }
+  }
+
+  dockScooter(scooter, station){
+      if(!this.stations[station]){
+        throw new Error('no such station')
+      }else if(this.stations[station].scooter ){
+        throw  Error('scooter already at station')
+      } else{
+        Scooter.dock(station)
+        this.stations[station].push(scooter)
+        console.log('scooter is docked')
+      }   
+  }
+   //registeredUsers = {shami : { username: ‘shami1’ , password: ‘milo’ , age : 17, logginIn: true} ,sarai : { username: ‘sarai’ , password: ‘hamilton’ , age : 10, logginIn: false} }
+   //this.stations = {chicago : [ scoot1:{station:chicago, user:null} , scoot2:{station:chicago, user:null}] , brooklyn : [scoot4:{station:brooklyn,user:null}, scoot9:{station:brooklyn,user:null}]}
+  rentScooter(scooter, user){
+    if(scooter.station === 'null'){
+      throw new Error('scooter is already rented')
+    } 
+    let array = this.stations[scooter.station]
+    for(let i = 0; i< array.length ; i++){
+      if(array[i] === scooter)
+      this.stations[scooter.station].splice(i,1)
+      //why cant I just call Scooter.rent(user) here?
+       scooter.station = null;
+       scooter.user = user;
+    }
+  }
+
+  print(){
+    console.log(this.registerUsers)
+    for(station in this.stations){
+      console.log(`${station} : ${this.stations[station].length}`)
+    }
+  }
+}
+
+
+module.exports = ScooterApp
 
